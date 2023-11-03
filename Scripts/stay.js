09180aba3bb9b896a0cc5389e241d8726d7e15d7
Lori Wilson <W.lori03@gmail.com>
2023-11-02 22:01:15 -0400

    I have added the home page and the stay/overnightform

---
+++ Scripts/stay.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.onload(){
+    
+// This is the function to calculate the room Rate
+function getRoomRate(checkInDate, roomType){
+    // In season and out of season rates
+   let inSeasonRates = {
+    queen: 250,
+    king: 250,
+    suite: 350
+   };
+
+   let outOfSeasonRates = {
+    queen: 150,
+    king: 150,
+    suite: 210
+   };
+
+}
+}
